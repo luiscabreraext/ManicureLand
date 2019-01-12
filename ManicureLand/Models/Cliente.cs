a095e07730ee3caaf04c6b8b86b64075a1feda2f
Luis Cabrera <lcabrera.ext@tanner.cl>
2019-01-12 11:27:32 -0300

    se agregan validaciones

--- ManicureLand/Models/Cliente.cs
+++ ManicureLand/Models/Cliente.cs
@@ -10,19 +10,48 @@ namespace ManicureLand.Models
     public class Cliente
     {
         public int Id { get; set; }
+
+        [DisplayName("Nombres")]
+        [StringLength(64), Required(ErrorMessage = "Campo Requerido")]
+        [RegularExpression(@"^[a-zA-Z\s\']+$", ErrorMessage = "Solo puede ingresar letras")]
         public string Nombres { get; set; }
+
         [DisplayName("Apellido Paterno")]
-        [StringLength(32), Required]
+        [StringLength(32), Required(ErrorMessage = "Campo Requerido")]
+        [RegularExpression(@"^[a-zA-Z\s\']+$", ErrorMessage = "Solo puede ingresar letras")]
         public string ApellidoPaterno { get; set; }
+
+        [DisplayName("Apellido Materno")]
+        [StringLength(32)]
+        [RegularExpression(@"^[a-zA-Z\s\']+$", ErrorMessage = "Solo puede ingresar letras")]
         public string ApellidoMaterno { get; set; }
 
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Debe indicar la fecha correcta")]
         public DateTime FechaNacimiento { get; set; }
 
+        [DataType(DataType.EmailAddress, ErrorMessage = "Ingrese un correo correctamente.")]
+        [StringLength(255), Required(ErrorMessage = "Campo Requerido")]
+        [RegularExpression(@"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$",
+                   ErrorMessage = "Formato de correo incorrecto de telefono, use un formato algo@algo.com")]
         public string Correo { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9]{1}[a-zA-Z0-9!@#\$\*&]{3,31}$",
+                  ErrorMessage = "Su contraseña debe empezar por una letra seguida de números, letras o los simbolos !, @, #, $, &, o * con un mínimo de 4 caracteres y máximo 32.")]
+        [Compare("RepetirClave", ErrorMessage = "Las constraseñas no coinciden")]
         public string Clave { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9]{1}[a-zA-Z0-9!@#\$\*&]{3,31}$",
+                  ErrorMessage = "Su contraseña debe empezar por una letra seguida de números, letras o los simbolos !, @, #, $, &, o * con un mínimo de 4 caracteres y máximo 32.")]
+                public string RepetirClave { get; set; }
+
+        [RegularExpression(@"^[\+]{1}[0-9]{11}$",
+                   ErrorMessage = "Formato incorrecto de telefono, use +56212345678.")]
         public string TelefonoFijo { get; set; }
+
+        [RegularExpression(@"^[\+]{1}[0-9]{11}$",
+                   ErrorMessage = "Formato incorrecto de telefono, use +56912345678.")]
         public string TelefonoMovil { get; set; }
 
         [DataType(DataType.Date)]
